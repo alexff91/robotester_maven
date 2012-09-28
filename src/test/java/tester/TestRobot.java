8796d601be3a533f5bdd50dec357abeeb7152696
Alex <alexff91@gmail.com>
2012-09-29 01:01:02 +0400

    trying to find the answer

--- src/test/java/tester/TestRobot.java
+++ src/test/java/tester/TestRobot.java
@@ -12,7 +12,8 @@ import org.openqa.selenium.support.ui.ExpectedCondition;
 
 import java.util.LinkedList;
 import java.util.List;
-
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class TestRobot {
     @Test
@@ -22,17 +23,14 @@ public class TestRobot {
             // Notice that the remainder of the code relies on the interface,
             // not the implementation.
             WebDriver driver = new HtmlUnitDriver();
-            driver.get("http://www.yandex.ru");
+            driver.get("http://market.yandex.ru/search?hid=7812186");
 
-            WebElement searchBox = driver.findElement(By.name("text"));
-            searchBox.sendKeys("стажировка");
-            searchBox.sendKeys(Keys.RETURN);
 
             List<List<WebElement>> elements = findAllElements(driver);
             for (List<WebElement> e : elements) {
-                System.out.println("<--------------------" + e.toString() + "---------------------->");
+                System.out.println("<--------------------" + e.getClass() + "---------------------->");
                 for (WebElement elem : e) {
-                    System.out.println(elem.getTagName()+ elem.getText());
+                    System.out.println(elem.toString()+ elem.getText());
                 }
             }
         } catch (Exception e) {
@@ -57,16 +55,29 @@ public class TestRobot {
     }
 
     private List<List<WebElement>> findAllElements(WebDriver driver) {
-        List<WebElement> allLinks = driver.findElements(By.xpath("//*"));
-
-        List<WebElement> inputs = driver.findElements(By.tagName("input"));
+        List<WebElement> checkBox = driver.findElements(By.xpath("//input[@type='checkbox']"));
+//        List<WebElement> foo = driver.findElements(By.tagName("a"));
+//        for(WebElement element : foo){
+//            String link = element.getAttribute("src");
+//        }
+        List<WebElement> inputsSearch = driver.findElements(By.xpath("//input"));
         List<WebElement> links = driver.findElements(By.tagName("a"));
-        List<WebElement> allElems = driver.findElements(By.xpath("//DIV[@id='someId']//A[@class='someClass']"));
+        List<WebElement> buttons = driver.findElements(By.xpath("//button"));
         List<List<WebElement>> l = new LinkedList<List<WebElement>>();
-        l.add(allElems);
-        l.add(allLinks);
+        l.add(buttons);
+       l.add(checkBox);
         l.add(links);
-        l.add(inputs);
+        l.add(inputsSearch);
         return l;
+    }
+    private void find(WebDriver driver){
+        String htmlSource = driver.getPageSource();
+        Pattern linkElementPattern = Pattern.compile("<ab[^>]*href=\"[^>]*>(.*?)</a>");
+        Matcher linkElementMatcher = linkElementPattern.matcher(htmlSource);
+        while (linkElementMatcher.find()) {
+            System.out.println(linkElementMatcher.group());
+        }
+        Object thisurl = driver.getCurrentUrl();
+        System.out.println(thisurl);
     }
 }
